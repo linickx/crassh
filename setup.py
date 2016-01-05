5df136b952eb81a3b057db5984ae09d7e1442951
Nick <linickx@gmail.com>
2016-01-05 16:10:04 +0000

    ImportError: No module named 'crassh'
    Build Failed, trying
    http://stackoverflow.com/questions/25756134/how-to-add-my-module-to-trav
    is-ci-pythonpath

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='CraSSH',
+      version='1.16-dev',
+      description='Cisco Remote Automation via SSH (or C.R.A.SSH for short) is a python script for automating commands on Cisco devices.',
+      author='Nick Bettison',
+      url='https://github.com/linickx/crassh/',
+     )
