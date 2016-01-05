8c76bacc30cbdfce62bbef1c3a1ec915646aafde
Nick <linickx@gmail.com>
2016-01-05 17:33:04 +0000

    Test the Do No Harm Function
    +2 Tests.

--- tests/test_crassh.py
+++ tests/test_crassh.py
@@ -11,3 +11,22 @@ def test_help(tmpdir, capsys):
     helpfile = tmpdir.mkdir("sub").join("help.txt")
     helpfile.write(out)
     assert helpfile.read() == out
+
+def test_dnh_ok():
+    # Check that dnh doesn't trip on good commands
+    good_cmds = ["write mem"]
+    for cmd in good_cmds:
+        # if the function exists then we're in trouble and an assert will automagically be raised.
+        crassh.do_no_harm(cmd)
+
+def test_dnh_evil(capsys):
+    # Check for Evil things
+    evil_cmds = ["reload", "reload in 5", "write erase", "wr er", "del flash:/*", "delete system:running-config"]
+    for cmd in evil_cmds:
+        # If the function exits then that is good!
+        with pytest.raises(SystemExit) as excinfo:
+            crassh.do_no_harm(cmd)
+        # https://pytest.org/latest/assert.html#assertions-about-expected-exceptions
+        # print(excinfo.value)
+        # The exit value must be 0
+        assert str(excinfo.value) == "0"
