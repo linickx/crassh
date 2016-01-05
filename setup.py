ec8b03039be3bf9461671749234631ed274ee781
Nick <linickx@gmail.com>
2016-01-05 16:25:33 +0000

    trying to fix module import on build
    Successfully installed CraSSH-1.16.dev0 but ImportError: No module
    named crassh

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='CraSSH',
       description='Cisco Remote Automation via SSH (or C.R.A.SSH for short) is a python script for automating commands on Cisco devices.',
       author='Nick Bettison',
       url='https://github.com/linickx/crassh/',
+      py_modules=['crassh'],
      )
