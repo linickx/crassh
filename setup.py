43e1a14984483a17523c2bb0abde259e6762aef4
Nick <linickx@gmail.com>
2016-02-11 14:27:34 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 from distutils.core import setup
 
 setup(name='CraSSH',
-      version='1.16-dev',
+      version='2.0',
       description='Cisco Remote Automation via SSH (or C.R.A.SSH for short) is a python script for automating commands on Cisco devices.',
       author='Nick Bettison',
       url='https://github.com/linickx/crassh/',
       py_modules=['crassh'],
+      install_requires=['paramiko >= 1.10'],
      )
