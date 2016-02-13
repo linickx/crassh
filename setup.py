f8724a793eb75f17b1f5eadd1dbaf399efdb097d
Nick <linickx@gmail.com>
2016-02-13 12:59:35 +0000

    version bump 2.02

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='CraSSH',
-      version='2.01',
+      version='2.02',
       description='Cisco Remote Automation via SSH (or C.R.A.SSH for short) is a python script for automating commands on Cisco devices.',
       author='Nick Bettison',
       url='https://github.com/linickx/crassh/',
