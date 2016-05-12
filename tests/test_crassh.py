71305ef02558183eade92711c360555067af6352
Nick <linickx@gmail.com>
2016-05-12 16:33:16 +0100

    starting to pylint the tests

--- tests/test_crassh.py
+++ tests/test_crassh.py
@@ -11,8 +11,12 @@
 
 """
 
+import os                       # ... standard Deps
+import stat
+import sys
+import pytest                   # Pytest
 import crassh                   # CraSSH!
-import os, pytest, stat, sys    # Deps
+
 
 # Current Directory
 CUR_DIR = os.path.dirname(__file__)
@@ -25,12 +29,14 @@ cisco = pytest.mark.skipif(
 
 """
     Function / Code Tests - Self Contained.
-    
+
     Naming Convention: Test_FunctionName.
 """
 
 def test_help(tmpdir, capsys):
-    # This test is mainly used for syntax validation. If the help is printed and written to a text file then the script syntax is both Python 2 & Python 3.
+    """ This test is mainly used for syntax validation.
+    If the help is printed and written to a text file then the script syntax is both Python 2 & Python 3.
+    """
     with pytest.raises(SystemExit):
         crassh.print_help()
     out, err = capsys.readouterr()
@@ -39,14 +45,16 @@ def test_help(tmpdir, capsys):
     assert helpfile.read() == out
 
 def test_dnh_ok():
-    # Check that dnh doesn't trip on good commands
+    """ Check that dnh doesn't trip on good commands
+    """
     good_cmds = ["write mem"]
     for cmd in good_cmds:
         # if the function exists then we're in trouble and an assert will automagically be raised.
         crassh.do_no_harm(cmd)
 
 def test_dnh_evil(capsys):
-    # Check for Evil things
+    """ Check for Evil things
+    """
     evil_cmds = ["reload", "rel", "reload in 5", "write erase", "wr er", "del flash:/*", "delete system:running-config"]
     for cmd in evil_cmds:
         # If the function exits then that is good!
@@ -60,25 +68,28 @@ def test_dnh_evil(capsys):
         assert str(excinfo.value) == "0"
 
 def test_isgroupreadable(tmpdir):
-    # Check out file permission function works - Groups
+    """ Check out file permission function works - Groups
+    """
     test_groupfile = tmpdir.mkdir("sub").join("groupfile.txt")
     test_groupfile.write("text")
     os.chmod(str(test_groupfile), stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP) # Chmod 640
-    assert crassh.isgroupreadable(str(test_groupfile)) == True
-    
+    assert crassh.isgroupreadable(str(test_groupfile)) is True
+
 def test_isotherreadable(tmpdir):
-    # Check out file permission function works - others 
+    """ Check out file permission function works - others
+    """
     test_othfile = tmpdir.mkdir("sub").join("otherfile.txt")
     test_othfile.write("text")
     os.chmod(str(test_othfile), stat.S_IRUSR | stat.S_IWUSR | stat.S_IROTH) # Chmod 604
-    assert crassh.isotherreadable(str(test_othfile)) == True
+    assert crassh.isotherreadable(str(test_othfile)) is True
 
 def test_readtxtfile(tmpdir):
-    # Test that a text file is read and stripped correctly.
+    """ Test that a text file is read and stripped correctly.
+    """
     test_input = ["1.1.1.1", " 1.1.1.2", "1.1.1.3 ", " 1.1.1.4 "]
-    ExpectedOutput = ["1.1.1.1", "1.1.1.2", "1.1.1.3", "1.1.1.4" ]
+    ExpectedOutput = ["1.1.1.1", "1.1.1.2", "1.1.1.3", "1.1.1.4"]
     test_file = tmpdir.mkdir("sub").join("file.txt")
-    f = open(str(test_file),'a')
+    f = open(str(test_file), 'a')
     for y in test_input:
         print(y)
         f.write(y + "\n")
@@ -89,9 +100,10 @@ def test_readtxtfile(tmpdir):
         counter += 1
 
 def test_readauthfile(tmpdir):
-    # Test reading credentials from a file
+    """ Test reading credentials from a file
+    """
     test_file = tmpdir.mkdir("sub").join("credz.txt")
-    f = open(str(test_file),'a')
+    f = open(str(test_file), 'a')
     f.write(" username:nick\n")
     f.write(" password :  pass  \n")
     f.close()
@@ -109,11 +121,13 @@ def test_readauthfile(tmpdir):
 """
 @cisco
 def test_cisco_main_shver_multi(capsys):
+    """ Test Show ver (main function) against multiple devices
+    """
     global sys
     SwitchFile = CUR_DIR + "/cisco_main_shver_multi_s.txt"         # IP Address of Switch/Router (for CLI Input)
     CmdFile = CUR_DIR + "/cisco_main_shver_multi_cmd.txt"          # The Command to run "show ver"
     OutputFile = CUR_DIR + "/cisco_main_shver_multi_output.txt"    # The expected output (from a "show ver")
-    f=open(OutputFile,'r')
+    f = open(OutputFile, 'r')
     ExpectedOutput = f.readlines()
     sys.argv[1:] = ['-U', 'nick', '-P', 'nick', '-p', '-s', SwitchFile, '-c', CmdFile] # ./crassh -U nick -P nick -p -s SwitchFile -c CmdFile
     crassh.main()
@@ -126,7 +140,8 @@ def test_cisco_main_shver_multi(capsys):
 
 @cisco
 def test_cisco_connect():
-    # Check that connection funtion returns expected hostname
+    """ Check that connection funtion returns expected hostname
+    """
     device = "1.1.1.2"
     username = "nick"
     password = "nick"
@@ -136,7 +151,8 @@ def test_cisco_connect():
 
 @cisco
 def test_cisco_connect_enable():
-    # Check that connection funtion returns expected hostname
+    """ Check that connection funtion returns expected hostname
+    """
     device = "1.1.1.2"
     username = "cisco"
     password = "cisco"
@@ -148,11 +164,13 @@ def test_cisco_connect_enable():
 
 @cisco
 def test_cisco_main_quit_default(capsys):
+    """ Test main function quits properly against a router
+    """
     global sys
     SwitchFile = CUR_DIR + "/cisco_main_quit_s.txt"         # IP Address of Switch/Router (for CLI Input)
     CmdFile = CUR_DIR + "/cisco_main_shver_multi_cmd.txt"          # The Command to run "show ver"
     OutputFile = CUR_DIR + "/cisco_main_quit_default_output.txt"    # The expected output (from a "show ver")
-    f=open(OutputFile,'r')
+    f = open(OutputFile, 'r')
     ExpectedOutput = f.readlines()
     sys.argv[1:] = ['-U', 'nick', '-P', 'nick', '-p', '-s', SwitchFile, '-c', CmdFile] # ./crassh -U nick -P nick -p -s SwitchFile -c CmdFile
     with pytest.raises(SystemExit):
@@ -165,11 +183,13 @@ def test_cisco_main_quit_default(capsys):
 
 @cisco
 def test_cisco_main_quit_disable(capsys):
+    """ Test main function quit can be disabled (against a router)
+    """
     global sys
     SwitchFile = CUR_DIR + "/cisco_main_quit_s.txt"         # IP Address of Switch/Router (for CLI Input)
     CmdFile = CUR_DIR + "/cisco_main_shver_multi_cmd.txt"          # The Command to run "show ver"
     OutputFile = CUR_DIR + "/cisco_main_quit_disable_output.txt"    # The expected output (from a "show ver")
-    f=open(OutputFile,'r')
+    f = open(OutputFile, 'r')
     ExpectedOutput = f.readlines()
     sys.argv[1:] = ['-U', 'nick', '-P', 'nick', '-p', '-s', SwitchFile, '-c', CmdFile, '-Q'] # ./crassh -U nick -P nick -p -s SwitchFile -c CmdFile -Q
     crassh.main()
